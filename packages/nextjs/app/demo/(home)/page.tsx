77492e670b9e07ee31245500055c3b06aea05871
Bora Lee <learnerbr@gmail.com>
2024-11-08 18:17:53 +0700

    update home UI

--- packages/nextjs/app/demo/(home)/page.tsx
+++ packages/nextjs/app/demo/(home)/page.tsx
@@ -3,22 +3,22 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-    <div className="flex w-full min-h-full mt-[125px] mb-[125px] justify-center items-center bg-[#e6e6e6]">
+    <div className="flex w-full min-h-full mt-[125px] mb-[125px] justify-center items-center">
       <div className="flex flex-col justify-start items-center gap-6 w-full overflow-y-auto mt-[-120px]">
         <div className="w-4/5 ">
-          <div className="w-full rounded-2xl bg-[#fafafa] flex justify-center items-center p-4 aspect-h-1">
+          <div className="w-full rounded-2xl flex justify-center items-center p-4 aspect-h-1">
             <Image
               width={250}
               height={250}
-              src="/gold-key.png"
+              src="/demo/gold-key.png"
               alt="Square"
               className="w-full h-full object-cover"
             />
           </div>
         </div>
 
-        <button className="w-4/5 px-4 py-2 bg-white text-[#FFDE59] text-lg rounded-2xl">
-          redeem key
+        <button className="border-2 border-[#9b94b3] w-4/5 px-4 py-2 text-[#9b94b3] text-lg font-semibold rounded-2xl">
+          unlock a key!
         </button>
       </div>
     </div>
