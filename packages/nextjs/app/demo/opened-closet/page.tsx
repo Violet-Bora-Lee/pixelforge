eed2c13ffdc3d1f21806bcfbc62ca1068c227158
Bora Lee <learnerbr@gmail.com>
2024-11-09 03:38:57 +0700

    remove margin top

--- packages/nextjs/app/demo/opened-closet/page.tsx
+++ packages/nextjs/app/demo/opened-closet/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
   };
 
   return (
-    <div className="flex flex-col w-full min-h-full mt-[125px] mb-[125px] justify-center items-center relative">
+    <div className="flex flex-col w-full min-h-full mb-[125px] justify-center items-center relative">
       <div className="flex flex-col justify-start items-center gap-2 w-full overflow-y-auto mt-[-10px]">
         <div className="relative w-full h-96 p-4">
           <div className="flex justify-between items-center h-full">
