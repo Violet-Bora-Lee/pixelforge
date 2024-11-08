89fe915ad3a91bf4f3542c52571accf24b40bfd5
Bora Lee <learnerbr@gmail.com>
2024-11-08 22:18:32 +0700

    mint new key integration

--- packages/nextjs/app/demo/unlock/page.tsx
+++ packages/nextjs/app/demo/unlock/page.tsx
@@ -1,20 +1,69 @@
 "use client";
 
-import React from "react";
-import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+import { useAutoConnect } from "~~/hooks/scaffold-stark";
+import { useAccount } from "~~/hooks/useAccount";
+
+import { useDeployedContractInfo } from '~~/hooks/scaffold-stark';
+import { useScaffoldReadContract } from "~~/hooks/scaffold-stark/useScaffoldReadContract";
+import { useScaffoldWriteContract } from '~~/hooks/scaffold-stark/useScaffoldWriteContract';
 
 export default function Page() {
+  useAutoConnect();
+
+  const { account: connectedAddress, address, status } = useAccount();
+
+
+  // // NOTE: workaround - check by name also since in starknet react devnet and sepolia has the same chainId
+  // const isLocalNetwork =
+  //   targetNetwork.id === devnet.id && targetNetwork.network === devnet.network;
+  // const isSepoliaNetwork =
+  //   targetNetwork.id === sepolia.id &&
+  //   targetNetwork.network === sepolia.network;
+  // const isMainnetNetwork =
+  //   targetNetwork.id === mainnet.id &&
+  //   targetNetwork.network === mainnet.network;
+
+
+  const { data: deployedContractData } = useDeployedContractInfo("WardrobeKeyBoredApes");
+
+  const { sendAsync: mintNewKey } = useScaffoldWriteContract({
+    contractName: "WardrobeKeyBoredApes",
+    functionName: "mint" as const,
+    args: [deployedContractData?.address]
+  });
+
+
+  const wrapInTryCatch =
+    (fn: () => Promise<any>, errorMessageFnDescription: string) => async () => {
+      try {
+        await fn();
+      } catch (error) {
+        console.error(
+          `Error calling ${errorMessageFnDescription} function`,
+          error,
+        );
+      }
+    };
+
+  useEffect(() => {
+    console.log("account: ", connectedAddress);
+    console.log("address: ", address);
+    console.log("status: ", status);
+  }, [connectedAddress]);
+
   return (
     <div className="flex w-full min-h-full mb-[125px] justify-center items-center">
       <div className="flex flex-col justify-start items-center gap-6 w-full overflow-y-auto mt-[-60px]">
         <div className="flex flex-col items-center w-full max-w-md space-y-2 px-6">
           <div className="space-y-2 w-full">
             <div className="text-center">
-              <h1 >Ready to unlock your key?</h1>
+              <h1>Ready to unlock your key?</h1>
               <p>Show us your NFT treasure!</p>
             </div>
 
-            <button className="border-2 border-[#9b94b3] w-full py-1 px-6 rounded-2xl text-gray-500 text-xl">
+            <button className="border border-[#9b94b3] w-full py-1 px-6 rounded-2xl text-gray-500 text-xl">
               unlock by nft
             </button>
           </div>
@@ -29,10 +78,13 @@ export default function Page() {
               <p className="text-xl text-center font-semibold">enter email</p>
               <input
                 type="email"
-                className="w-full py-1 px-6 rounded-2xl border border-gray-200"
+                className="w-full py-1 px-6 rounded-2xl border border-[#9b94b3] focus:border-[#9b94b3] focus:border focus:outline-none focus-visible:outline-none focus-visible:ring-0"
                 placeholder=""
               />
-              <button className="border-2 border-[#9b94b3] w-full py-1 px-6  rounded-2xl text-gray-500 text-xl">
+              <button 
+                className="border border-[#9b94b3] w-full py-1 px-6 rounded-2xl text-gray-500 text-xl"
+                onClick={wrapInTryCatch(mintNewKey, "mintNewKey")}
+                >
                 send magic link!
               </button>
             </div>
