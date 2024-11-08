8186fad51e3d2cbb185b67f6885380627bd709de
Bora Lee <learnerbr@gmail.com>
2024-11-09 02:33:30 +0700

    keyguide page

---
+++ packages/nextjs/app/demo/key-guide/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+
+export default function Page() {
+  const router = useRouter();
+
+  const goToKeyboxPage = () => {
+    router.push("/demo/keybox");
+  };
+
+  return (
+    <div className="flex w-full min-h-full mb-[125px] justify-center items-center">
+      <div className="flex flex-col justify-start items-center gap-6 w-full overflow-y-auto mt-[-60px]">
+        <div className="flex flex-col items-center w-full max-w-md space-y-2 px-6">
+          <div className="space-y-6 w-full">
+            <div className="text-center text-xl">
+              <p>congrats!</p>
+              <p>you’ve unlocked a key!</p>
+            </div>
+
+            <div className="text-center text-xl">
+              <p>your community is waiting</p>
+              <p>—explore your digital</p>
+              <p>wardrobe, show off your</p>
+              <p>affiliations & find new</p>
+              <p>friends along the way!</p>
+            </div>
+
+            <button
+              className="border border-[#9b94b3] w-full py-1 px-6 rounded-2xl text-gray-500 text-xl"
+              onClick={goToKeyboxPage}
+            >
+              see my keys
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- packages/nextjs/app/demo/redeemed-key/page.tsx
+++ packages/nextjs/app/demo/redeemed-key/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import React from "react";
 import Image from 'next/image';
-
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+
+  const goToRedeemedPage = () => {
+    router.push("/demo/key-guide");
+  };
+
   return (
-    <div className="flex w-full min-h-full mt-[125px] mb-[125px] justify-center items-center">
+    <div 
+      className="flex w-full min-h-full mt-[125px] mb-[125px] justify-center items-center"
+      onClick={goToRedeemedPage}
+    >
       <div className="flex flex-col justify-start items-center gap-6 w-full overflow-y-auto mt-[-120px]">
         <div className="w-4/5 ">
           <div className="w-full rounded-2xl flex justify-center items-center p-4 aspect-h-1">
