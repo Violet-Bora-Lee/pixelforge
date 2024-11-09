0e2ba87d0feb06ae0ef3783dd91b708528bfc4c4
Bora Lee <learnerbr@gmail.com>
2024-11-09 18:07:44 +0700

    smart contract (`mint` new key NFT with BAYC NFT) integration

--- packages/nextjs/app/demo/checking-wallet/page.tsx
+++ packages/nextjs/app/demo/checking-wallet/page.tsx
@@ -1,21 +1,65 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 
 import { useRouter } from 'next/navigation';
 
+import { useAutoConnect } from "~~/hooks/scaffold-stark";
+import { useAccount } from "~~/hooks/useAccount";
+
+import { useScaffoldWriteContract } from '~~/hooks/scaffold-stark/useScaffoldWriteContract';
+
 export default function Page() {
+  useAutoConnect();
+
+  const { account: connectedAccountInfo, address: connectedAddress, status } = useAccount();
+
+  
+  const { sendAsync: mintNewkeyWithBaycNft } = useScaffoldWriteContract({
+    contractName: "WardrobeKeyBoredApes",
+    functionName: "mint" as const,
+    args: [connectedAddress],
+  });
+
+  const wrapInTryCatch =
+  (fn: () => Promise<any>, errorMessageFnDescription: string) => async () => {
+    try {
+      await fn();
+    } catch (error) {
+      console.error(
+        `Error calling ${errorMessageFnDescription} function`,
+        error,
+      );
+    }
+  };
+
+
   const router = useRouter();
 
   const goToKeyMintedPage = () => {
     router.push('/demo/redeemed-key');
   };
 
+  useEffect(() => {
+    console.log("connected account info: ", connectedAccountInfo);
+    console.log("connected address: ", connectedAddress);
+    console.log("status: ", status);
+  }, [connectedAddress]);
+
+  const handleClick = () => {
+    wrapInTryCatch(
+      () => mintNewkeyWithBaycNft(),
+      "mintNewkeyWithBaycNft"
+    )().then(() => {
+      goToKeyMintedPage();
+    });
+  };
+
   return (
     <div 
-    className="flex w-full min-h-full mb-[125px] justify-center items-center"
-    onClick={goToKeyMintedPage}
+      className="flex w-full min-h-full mb-[125px] justify-center items-center"
+      onClick={handleClick}
     >
       <div className="flex flex-col justify-start items-center gap-6 w-full overflow-y-auto mt-[-60px]">
         <div className="flex flex-col items-center w-full max-w-md space-y-6 px-6">
