d4e9e2bf2acab26dae07bc48cde9bbceff7c2090
Bora Lee <learnerbr@gmail.com>
2024-11-10 01:24:38 +0700

    add router

--- packages/nextjs/app/demo/opened-closet/page.tsx
+++ packages/nextjs/app/demo/opened-closet/page.tsx
@@ -11,7 +11,10 @@ export default function Page() {
   };
 
   return (
-    <div className="flex flex-col w-full min-h-full mb-[125px] justify-center items-center relative">
+    <div
+      className="flex flex-col w-full min-h-full mb-[125px] justify-center items-center relative"
+      onClick={goToAvatarDecoratePage}
+    >
       <div className="flex flex-col justify-start items-center gap-2 w-full overflow-y-auto mt-[-10px]">
         <div className="relative w-full h-96 p-4">
           <div className="flex justify-between items-center h-full">
