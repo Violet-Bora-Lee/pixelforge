6df966fd64c57c0d13fb28acb0aecc1128d6bbf8
Bora Lee <learnerbr@gmail.com>
2024-11-10 01:21:58 +0700

    modify margin

--- packages/nextjs/app/demo/(home)/page.tsx
+++ packages/nextjs/app/demo/(home)/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
   };
 
   return (
-    <div className="flex w-full min-h-full mt-[125px] mb-[125px] justify-center items-center">
+    <div className="flex w-full min-h-full mt-[30px] mb-[125px] justify-center items-center">
       <div className="flex flex-col justify-start items-center gap-6 w-full overflow-y-auto mt-[-120px]">
         <div className="w-4/5 ">
           <div className="w-full rounded-2xl flex justify-center items-center p-4 aspect-h-1">
