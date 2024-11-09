533b05a84300514e68d9663fad4ce00834ed3bf3
Bora Lee <learnerbr@gmail.com>
2024-11-09 17:20:00 +0700

    checking wallet UI

---
+++ packages/nextjs/app/demo/checking-wallet/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+import { useRouter } from 'next/navigation';
+
+export default function Page() {
+  const router = useRouter();
+
+  const goToKeyMintedPage = () => {
+    router.push('/demo/redeemed-key');
+  };
+
+  return (
+    <div 
+    className="flex w-full min-h-full mb-[125px] justify-center items-center"
+    onClick={goToKeyMintedPage}
+    >
+      <div className="flex flex-col justify-start items-center gap-6 w-full overflow-y-auto mt-[-60px]">
+        <div className="flex flex-col items-center w-full max-w-md space-y-6 px-6">
+          <div className="text-center text-xl">
+            <p className="flex flew-col w-full space-x-2">
+              <span>just a sec</span>
+              <Image
+                width={32}
+                height={32}
+                className="object-contain"
+                src="/demo/loading-dots.gif"
+                alt="loading dots"
+              />
+            </p>
+          </div>
+          <div className="text-center text-xl">
+            <p>we are checking</p>
+            <p>your wallet to find</p>
+            <p>that treasure...</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
